6c68602b1e4a9dc4140f9a35c1b592083a10ec84
Christoffer Lind Andersen <christoeu@pop-os.localdomain>
2020-10-20 19:04:56 +0200

    Test

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 const test: number[] = [123, 3123, 414, 91, 9818, 99, 1, 10000]
 console.log(insertionSort(test))
 
+const string: string = 'hello'
+
 function insertionSort (array: number[]) {
   for (let i = 1; i < array.length; i++) {
     const key: number = array[i]
