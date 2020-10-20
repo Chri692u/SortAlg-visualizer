98c77380d0a194f2145d1ebf058e724320e43882
Andreas Hummelmose <hummelmosea@gmail.com>
2020-10-20 19:08:43 +0200

    Slider added random array

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 const test: number[] = [123, 3123, 414, 91, 9818, 99, 1, 10000]
 console.log(insertionSort(test))
 
-function insertionSort (array: number[]) {
+function insertionSort(array: number[]) {
   for (let i = 1; i < array.length; i++) {
     const key: number = array[i]
     let j: number = i - 1
@@ -12,4 +12,25 @@ function insertionSort (array: number[]) {
     array[j + 1] = key
   }
   return array
+}
+
+function bingoSort(arr: number[]) {
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min: number = i;
+    for (let j = i + 1; j < arr.length; j++) {
+
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      swap(arr, min, i)
+    }
+  }
+
+}
+
+function swap(arr: number[], indexOne: number, indexTwo: number) {
+  [arr[indexOne], arr[indexTwo]] = [arr[indexTwo], arr[indexOne]];
 }
