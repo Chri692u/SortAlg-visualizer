b409ed9243f8fbe7c0c231ee392bfb34e34777b0
Andreas Hummelmose <hummelmosea@gmail.com>
2020-10-22 21:41:49 +0200

    kill me

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,16 @@ module.exports = {
       {
         test: /\.ts$/,
         use: 'ts-loader',
+        exclude: [path.resolve(__dirname, 'node_modules')],
         include: [path.resolve(__dirname, 'src')]
       }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   }
-
 }
