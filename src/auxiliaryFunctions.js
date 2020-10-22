9db67c45d78386ea3a8da84781f01ba407282515
Andreas Hummelmose <hummelmosea@gmail.com>
2020-10-22 22:40:44 +0200

    It works

--- src/auxiliaryFunctions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.swap = void 0;
-function swap(arr, indexOne, indexTwo) {
-    var _a;
-    _a = [arr[indexTwo], arr[indexOne]], arr[indexOne] = _a[0], arr[indexTwo] = _a[1];
-}
-exports.swap = swap;
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,26 @@
 import { Algorithms } from './Algorithms' // Sorting algorithm implementation class
+
+const randomArray = (val: number) => {
+  return Array.from({ length: val }, () => Math.floor((Math.random() * 10) + 1))
+}
+
+const slider = <HTMLInputElement>document.getElementById('myRange')
+const output: HTMLElement = document.getElementById('array-size')!
+const sortAlg: any = document.getElementById('options')
+const btnID: HTMLElement = document.getElementById('btnClick')!
+let arr = randomArray(1)
+
+output.innerHTML = `Size of array: ${slider.value}`
+
+slider.oninput = function () {
+  output.innerHTML = `Size of array: ${slider.value}`
+  arr = randomArray(Number(slider.value))
+}
+
+btnID.onclick = function (this: any) {
+  console.log(visualizer(Number(sortAlg.value), arr))
+}
+
 function visualizer (val: number, arr: number[]) {
   switch (Number(val)) {
     case 1:
@@ -9,26 +31,3 @@ function visualizer (val: number, arr: number[]) {
       return Algorithms.bingoSort(arr)
   }
 }
-
-// function createArrayFromSlider () {
-//   const randomArray = (val: number) => {
-//     return Array.from({ length: val }, () => Math.floor((Math.random() * 10) + 1))
-//   }
-
-//   const slider = <HTMLInputElement>document.getElementById('myRange')
-//   const output: HTMLElement = document.getElementById('array-size')!
-//   const sortAlg: any = document.getElementById('options')
-//   const btnID: HTMLElement = document.getElementById('btnClick')!
-//   let arr = randomArray(1)
-
-// //   output.innerHTML = `Size of array: ${slider.value}`
-
-//   slider.oninput = function (this: any) {
-//     output.innerHTML = `Size of array: ${this.value}`
-//     arr = randomArray(this.value)
-//   }
-
-//   btnID.onclick = function (this: any) {
-//     console.log(visualizer(Number(sortAlg.value), arr))
-//   }
-// }
