04871abae987bea13889b02291668c270464ac3b
Christoffer Lind Andersen <christoeu@pop-os.localdomain>
2020-10-22 21:41:06 +0200

    Moved bits around

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Algorithms } from './Algorithms'
+import { Algorithms } from './Algorithms' // Sorting algorithm implementation class
 
 function visualizer (val: number, arr: number[]) {
   switch (Number(val)) {
@@ -8,5 +8,28 @@ function visualizer (val: number, arr: number[]) {
       return Algorithms.doubleSelectionSort(arr)
     case 3:
       return Algorithms.bingoSort(arr)
+  }
+}
+
+function createArrayFromSlider () {
+  const randomArray = (val: number) => {
+    return Array.from({ length: val }, () => Math.floor((Math.random() * 10) + 1))
+  }
+
+  const slider = <HTMLInputElement>document.getElementById('myRange')
+  const output: HTMLElement = document.getElementById('array-size')!
+  const sortAlg: HTMLElement = document.getElementById('options')!
+  const btnID: HTMLElement = document.getElementById('btnClick')!
+  let arr = randomArray(1)
+
+  output.innerHTML = `Size of array: ${slider.value}`
+
+  slider.oninput = function () {
+    output.innerHTML = `Size of array: ${this.value}`
+    arr = randomArray(this.value)
+  }
+
+  btnID.onclick = function () {
+    console.log(visualizer(Number(sortAlg.value), arr))
   }
 }
