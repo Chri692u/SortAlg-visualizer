16b4adfe785963b6a8279f0e4769113deb9b944f
Andreas Hummelmose <hummelmosea@gmail.com>
2020-10-23 18:14:04 +0200

    fixed text align and color after merge

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const slider = <HTMLInputElement>document.getElementById('myRange')
 const output: HTMLElement = document.getElementById('array-size')!
 const btnID: HTMLElement = document.getElementById('btnClick')!
 const columns: HTMLElement = document.getElementById('columns')!
+const options = <HTMLInputElement>document.getElementById('options')!
 output.innerHTML = `Size of array: ${slider.value}`
 let arr = randomArray(Number(slider.value))
 
@@ -16,30 +17,28 @@ slider.oninput = function () {
   createArray(arr, 'red')
 }
 
-// btnID.onclick = function () {
-//   createArray(visualizer(sortAlg.value, arr), 'green')
-// }
+btnID.onclick = function () {
+  createArray(visualizer(options.value, arr), 'green')
+}
 
 // Functions
-// function visualizer (val: string, arr: number[]) {
-//   switch (Number(val)) {
-//     case 1:
-//       return Algorithms.insertionSort(arr)
-//     case 2:
-//       return Algorithms.doubleSelectionSort(arr)
-//     case 3:
-//       return Algorithms.bingoSort(arr)
-//   }
-// }
+function visualizer (val: string, arr: number[]) {
+  switch (Number(val)) {
+    case 1:
+      return Algorithms.insertionSort(arr)
+    case 2:
+      return Algorithms.doubleSelectionSort(arr)
+    case 3:
+      return Algorithms.bingoSort(arr)
+  }
+}
 
 function createArray (arr: any, sort: string) : void {
   columns.innerHTML = ''
   for (let i = 0; i < arr.length; i++) {
     const div = document.createElement('div')
-    div.setAttribute('class', sort)
-    div.setAttribute('class', 'col')
+    div.setAttribute('class', `col ${sort}`)
     div.innerHTML = `${arr[i]}`
     columns.appendChild(div)
-    console.log(123)
   }
 }
