9f2a8852fa30d096285530f74b987637f1bae289
Andreas Hummelmose <hummelmosea@gmail.com>
2020-10-23 17:52:11 +0200

    eslint added

--- src/main.ts
+++ src/main.ts
@@ -9,26 +9,24 @@ const slider = <HTMLInputElement>document.getElementById('myRange')
 const output: HTMLElement = document.getElementById('array-size')!
 const sortAlg: any = document.getElementById('options')
 const btnID: HTMLElement = document.getElementById('btnClick')!
-const showArray: HTMLElement = document.getElementById('showArray')!
-const columns: HTMLElement = document.getElementById("columns")!
+const columns: HTMLElement = document.getElementById('columns')!
 output.innerHTML = `Size of array: ${slider.value}`
 let arr = randomArray(Number(slider.value))
 
-createArray(arr,'red')
+createArray(arr, 'red')
 
 slider.oninput = function () {
   output.innerHTML = `Size of array: ${slider.value}`
-   arr = randomArray(Number(slider.value))
-  createArray(arr,'red')
+  arr = randomArray(Number(slider.value))
+  createArray(arr, 'red')
 }
 
 btnID.onclick = function () {
-  createArray(visualizer(sortAlg.value,arr), 'green')
+  createArray(visualizer(sortAlg.value, arr), 'green')
 }
 
 // Functions
 function visualizer (val: string, arr: number[]) {
-
   switch (Number(val)) {
     case 1:
       return Algorithms.insertionSort(arr)
@@ -37,15 +35,13 @@ function visualizer (val: string, arr: number[]) {
     case 3:
       return Algorithms.bingoSort(arr)
   }
-  
 }
-
 
 function createArray (arr: any, sort: string) : void {
   columns.innerHTML = ''
-  for(let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     const div = document.createElement('div')
-    div.setAttribute('class',sort)
+    div.setAttribute('class', sort)
     div.innerHTML = `${arr[i]}`
     columns.appendChild(div)
     console.log(123)
